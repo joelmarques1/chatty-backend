fa304ef9a2a909cd237cdd993c957b6eb2233859
Joel <50145954+joelmarques1@users.noreply.github.com>
2025-03-27 02:53:19 +0000

    Setup Project finished

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -11,9 +11,9 @@ import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
 import Logger from 'bunyan';
 import 'express-async-errors';
-import { config } from './config';
-import applicatoinRoutes from './routes';
-import IErrorResponse, { CustomError } from './shared/globals/helpers/error-handler';
+import { config } from '@root/config';
+import applicatoinRoutes from '@root/routes';
+import IErrorResponse, { CustomError } from '@global/helpers/error-handler';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.creatLogger('server');
